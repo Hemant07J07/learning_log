1f49783699a1cc1bf4449f4822bcc7c97d8446c5
hdubey <hdubey156@gmail.com>
2025-04-17 14:32:16 +0530

    Fixes

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -145,22 +145,4 @@ LOGIN_URL = '/users/login/'
 BOOTSTRAP3 = {
 	'include_jquery': True,
 }
-
-# Heroku setings
-if os.getcwd() == '/app':
-	import dj_database_url
-	DATABASES = {
-		'default': dj_database_url.config(default='postgres://localhost')
-	}
-	# Honor the 'X-Forwarded-Proto' header for request.is_secure().
-	SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO' , 'https')
-
-	# Allow all host headers.
-	ALLOWED_HOSTS = ['*']
-
-	# Static asset configuration
-	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-	STATIC_ROOT = 'staticfiles'
-	SATICFILES_DIRS = (
-		os.path.join(BASE_DIR, 'static'),
-	)
+ALLOWED_HOSTS = ['*']
